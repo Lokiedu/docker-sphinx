e5d691ac830afc1811c2c53979eea56b6cbd2915
Alexey Zakhlestin <indeyets@gmail.com>
2016-05-31 13:36:22 -0400

    Add initial indexer script

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM leodido/sphinxsearch:2.2.10
 
 COPY sphinx.conf /usr/local/etc/sphinx.conf
+COPY sphinx-index.sh /usr/bin/
 
 CMD ["searchd.sh"]
---
+++ sphinx-index.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Sphinx's searchd should be running for this to work
+
+indexer -c /usr/local/etc/sphinx.conf --all --rotate
+
+mysql -h0 -P9306 << SQL
+ATTACH INDEX Geotags TO RTINDEX GeotagsRT;
+ATTACH INDEX Hashtags TO RTINDEX HashtagsRT;
+ATTACH INDEX Posts TO RTINDEX PostsRT;
+ATTACH INDEX Users TO RTINDEX UsersRT;
+ATTACH INDEX Schools TO RTINDEX SchoolsRT;
+ATTACH INDEX Comments TO RTINDEX CommentsRT;
+SQL
